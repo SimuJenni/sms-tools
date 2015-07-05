6db0a15958de35370f899c036d80b450fd5d54ef
simujenni <simujenni@gmail.com>
2015-07-05 20:34:04 +0200

    Added magnitude spectrum computation

--- workspace/A2/A2Part5.py
+++ workspace/A2/A2Part5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from A2Part3 import DFT
 
 """
 A2-Part-5: Compute the magnitude spectrum (Optional)
@@ -21,5 +22,6 @@ def genMagSpec(x):
         magX (numpy array) = The magnitude spectrum of the input sequence x
                              (length N)
     """
-    ## Your code here
+    X = DFT(x)
+    return np.abs(X)
     
